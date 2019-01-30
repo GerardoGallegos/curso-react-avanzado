44583f0782672beb1955d43f61005de3be93f3c8
Gerardo Gallegos <puxerop@gmail.com>
2019-01-30 15:31:34 -0600

    Leccion # 3 - Usando destructuracion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
- return (
-   <div>
-     <h1>Ninja PRO</h1>
-   </div>
- )
+const Title = ({ uiColor, children }) => {
+  const styles = {
+    background: uiColor,
+    padding: '0.3em',
+    color: '#FFF',
+    borderRadius: '0.3em',
+    textAlign: 'center',
+    fontSize: '50px'
+  }
+
+  return (
+    <h1 style={styles}>
+      { children }
+    </h1>
+  )
+}
+
+
+class App extends Component {
+  state = {
+    uiColor: 'tomato'
+  }
+
+  render () {
+    const { uiColor } = this.state
+
+    return (
+      <div>
+        <Title
+          uiColor={uiColor}
+        >
+          Super <em>Ninja</em>
+        </Title>
+      </div>
+    )
+  }
 }
 
 export default App
